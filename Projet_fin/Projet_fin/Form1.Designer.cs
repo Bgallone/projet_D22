93693da692dbbb6821f7333bea0aa24aff9a336c
Stodin <flopo57230@hotmail.fr>
2016-05-05 20:11:12 +0200

    Entête

--- Projet_fin/Projet_fin/Form1.Designer.cs
+++ Projet_fin/Projet_fin/Form1.Designer.cs
@@ -28,112 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grbSection = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
-            this.label1 = new System.Windows.Forms.Label();
-            this.groupBox1.SuspendLayout();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.grbSection.SuspendLayout();
             this.SuspendLayout();
             // 
-            // checkBox1
+            // label1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(17, 45);
-            this.checkBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(98, 21);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Accueil";
+            // 
+            // grbSection
+            // 
+            this.grbSection.Controls.Add(this.button4);
+            this.grbSection.Controls.Add(this.button5);
+            this.grbSection.Controls.Add(this.label5);
+            this.grbSection.Controls.Add(this.label4);
+            this.grbSection.Controls.Add(this.button3);
+            this.grbSection.Controls.Add(this.label3);
+            this.grbSection.Controls.Add(this.label2);
+            this.grbSection.Controls.Add(this.button2);
+            this.grbSection.Controls.Add(this.label1);
+            this.grbSection.Controls.Add(this.button1);
+            this.grbSection.Location = new System.Drawing.Point(12, 12);
+            this.grbSection.Name = "grbSection";
+            this.grbSection.Size = new System.Drawing.Size(761, 94);
+            this.grbSection.TabIndex = 11;
+            this.grbSection.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(158, 103);
+            this.button1.Location = new System.Drawing.Point(6, 21);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(36, 21);
-            this.button1.TabIndex = 1;
+            this.button1.Size = new System.Drawing.Size(53, 55);
+            this.button1.TabIndex = 11;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Evénements";
+            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(160, 155);
+            this.button2.Location = new System.Drawing.Point(154, 21);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(34, 21);
-            this.button2.TabIndex = 2;
+            this.button2.Size = new System.Drawing.Size(53, 55);
+            this.button2.TabIndex = 12;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(160, 45);
+            this.button3.Location = new System.Drawing.Point(300, 21);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(34, 21);
-            this.button3.TabIndex = 3;
+            this.button3.Size = new System.Drawing.Size(53, 55);
+            this.button3.TabIndex = 12;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // checkBox2
+            // label3
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(17, 103);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(98, 21);
-            this.checkBox2.TabIndex = 4;
-            this.checkBox2.Text = "checkBox2";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(359, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Participants";
             // 
-            // checkBox3
+            // button4
             // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(17, 156);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(98, 21);
-            this.checkBox3.TabIndex = 5;
-            this.checkBox3.Text = "checkBox3";
-            this.checkBox3.UseVisualStyleBackColor = true;
+            this.button4.Location = new System.Drawing.Point(447, 21);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(53, 55);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
             // 
-            // groupBox1
+            // label4
             // 
-            this.groupBox1.Controls.Add(this.checkBox1);
-            this.groupBox1.Controls.Add(this.button3);
-            this.groupBox1.Controls.Add(this.checkBox3);
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.checkBox2);
-            this.groupBox1.Location = new System.Drawing.Point(516, 154);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 225);
-            this.groupBox1.TabIndex = 6;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(506, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 17);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Dépenses";
             // 
-            // label1
+            // button5
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(262, 107);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "label1";
+            this.button5.Location = new System.Drawing.Point(595, 21);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(53, 55);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(654, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 17);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Bilan";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(681, 534);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Tailleminlab";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(851, 421);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(785, 570);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.grbSection);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
-            this.Text = "Bite poilue ";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.grbSection.ResumeLayout(false);
+            this.grbSection.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,15 +178,19 @@
 
         #endregion
 
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox3;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.HelpProvider helpProvider1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox grbSection;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+
     }
 }
 
--- Projet_fin/Projet_fin/Form1.cs
+++ Projet_fin/Projet_fin/Form1.cs
@@ -16,5 +16,7 @@ namespace Projet_fin
         {
             InitializeComponent();
         }
+
+       
     }
 }
