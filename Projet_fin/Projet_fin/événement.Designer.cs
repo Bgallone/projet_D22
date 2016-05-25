38d39aa121986e258f9eb621376c67fd56d57d93
Stodin <flopo57230@hotmail.fr>
2016-05-25 10:24:03 +0200

    TrucEvénement

---
+++ Projet_fin/Projet_fin/événement.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace Projet_fin
+{
+    partial class événement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TabControl tbcEvé;
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.LblEveDescri = new System.Windows.Forms.Label();
+            this.LblEveInt = new System.Windows.Forms.Label();
+            this.LblCréateur = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rdbRéglé = new System.Windows.Forms.RadioButton();
+            this.lblFini = new System.Windows.Forms.Label();
+            this.lblau = new System.Windows.Forms.Label();
+            this.lbldu = new System.Windows.Forms.Label();
+            this.lblDescri = new System.Windows.Forms.Label();
+            this.lblInti = new System.Windows.Forms.Label();
+            this.lblCrée = new System.Windows.Forms.Label();
+            this.btnFin = new System.Windows.Forms.Button();
+            this.btnSuiv = new System.Windows.Forms.Button();
+            this.btnPre = new System.Windows.Forms.Button();
+            this.BtnDebut = new System.Windows.Forms.Button();
+            this.pgb_Prog = new System.Windows.Forms.ProgressBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblNumChange = new System.Windows.Forms.Label();
+            this.lblNévé = new System.Windows.Forms.Label();
+            this.ptb_événement = new System.Windows.Forms.PictureBox();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lblDeb = new System.Windows.Forms.Label();
+            this.lblFin = new System.Windows.Forms.Label();
+            tbcEvé = new System.Windows.Forms.TabControl();
+            tbcEvé.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_événement)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbcEvé
+            // 
+            tbcEvé.Appearance = System.Windows.Forms.TabAppearance.FlatButtons;
+            tbcEvé.Controls.Add(this.tabPage3);
+            tbcEvé.Controls.Add(this.tabPage4);
+            tbcEvé.Location = new System.Drawing.Point(-6, -2);
+            tbcEvé.Name = "tbcEvé";
+            tbcEvé.SelectedIndex = 0;
+            tbcEvé.Size = new System.Drawing.Size(1239, 671);
+            tbcEvé.TabIndex = 1;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tabPage3.Controls.Add(this.lblFin);
+            this.tabPage3.Controls.Add(this.lblDeb);
+            this.tabPage3.Controls.Add(this.LblEveDescri);
+            this.tabPage3.Controls.Add(this.LblEveInt);
+            this.tabPage3.Controls.Add(this.LblCréateur);
+            this.tabPage3.Controls.Add(this.label1);
+            this.tabPage3.Controls.Add(this.rdbRéglé);
+            this.tabPage3.Controls.Add(this.lblFini);
+            this.tabPage3.Controls.Add(this.lblau);
+            this.tabPage3.Controls.Add(this.lbldu);
+            this.tabPage3.Controls.Add(this.lblDescri);
+            this.tabPage3.Controls.Add(this.lblInti);
+            this.tabPage3.Controls.Add(this.lblCrée);
+            this.tabPage3.Controls.Add(this.btnFin);
+            this.tabPage3.Controls.Add(this.btnSuiv);
+            this.tabPage3.Controls.Add(this.btnPre);
+            this.tabPage3.Controls.Add(this.BtnDebut);
+            this.tabPage3.Controls.Add(this.pgb_Prog);
+            this.tabPage3.Controls.Add(this.groupBox1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 28);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1231, 639);
+            this.tabPage3.TabIndex = 0;
+            this.tabPage3.Text = "tabPage3";
+            // 
+            // LblEveDescri
+            // 
+            this.LblEveDescri.AutoSize = true;
+            this.LblEveDescri.Location = new System.Drawing.Point(359, 291);
+            this.LblEveDescri.Name = "LblEveDescri";
+            this.LblEveDescri.Size = new System.Drawing.Size(148, 17);
+            this.LblEveDescri.TabIndex = 16;
+            this.LblEveDescri.Text = "Descri de l\'événement";
+            // 
+            // LblEveInt
+            // 
+            this.LblEveInt.AutoSize = true;
+            this.LblEveInt.Location = new System.Drawing.Point(359, 239);
+            this.LblEveInt.Name = "LblEveInt";
+            this.LblEveInt.Size = new System.Drawing.Size(137, 17);
+            this.LblEveInt.TabIndex = 15;
+            this.LblEveInt.Text = "Nom de l\'événement";
+            // 
+            // LblCréateur
+            // 
+            this.LblCréateur.AutoSize = true;
+            this.LblCréateur.Location = new System.Drawing.Point(359, 181);
+            this.LblCréateur.Name = "LblCréateur";
+            this.LblCréateur.Size = new System.Drawing.Size(238, 17);
+            this.LblCréateur.TabIndex = 14;
+            this.LblCréateur.Text = "Changer par le nom de l\'oganisateur";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "label1";
+            // 
+            // rdbRéglé
+            // 
+            this.rdbRéglé.AutoSize = true;
+            this.rdbRéglé.Enabled = false;
+            this.rdbRéglé.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbRéglé.Location = new System.Drawing.Point(331, 409);
+            this.rdbRéglé.Name = "rdbRéglé";
+            this.rdbRéglé.Size = new System.Drawing.Size(17, 16);
+            this.rdbRéglé.TabIndex = 12;
+            this.rdbRéglé.TabStop = true;
+            this.rdbRéglé.UseVisualStyleBackColor = true;
+            // 
+            // lblFini
+            // 
+            this.lblFini.AutoSize = true;
+            this.lblFini.Location = new System.Drawing.Point(252, 409);
+            this.lblFini.Name = "lblFini";
+            this.lblFini.Size = new System.Drawing.Size(53, 17);
+            this.lblFini.TabIndex = 11;
+            this.lblFini.Text = "Réglé :";
+            // 
+            // lblau
+            // 
+            this.lblau.AutoSize = true;
+            this.lblau.Location = new System.Drawing.Point(461, 355);
+            this.lblau.Name = "lblau";
+            this.lblau.Size = new System.Drawing.Size(32, 17);
+            this.lblau.TabIndex = 10;
+            this.lblau.Text = "au :";
+            // 
+            // lbldu
+            // 
+            this.lbldu.AutoSize = true;
+            this.lbldu.Location = new System.Drawing.Point(252, 355);
+            this.lbldu.Name = "lbldu";
+            this.lbldu.Size = new System.Drawing.Size(34, 17);
+            this.lbldu.TabIndex = 9;
+            this.lbldu.Text = "Du :";
+            // 
+            // lblDescri
+            // 
+            this.lblDescri.AutoSize = true;
+            this.lblDescri.Location = new System.Drawing.Point(252, 291);
+            this.lblDescri.Name = "lblDescri";
+            this.lblDescri.Size = new System.Drawing.Size(87, 17);
+            this.lblDescri.TabIndex = 8;
+            this.lblDescri.Text = "Description :";
+            // 
+            // lblInti
+            // 
+            this.lblInti.AutoSize = true;
+            this.lblInti.Location = new System.Drawing.Point(252, 239);
+            this.lblInti.Name = "lblInti";
+            this.lblInti.Size = new System.Drawing.Size(57, 17);
+            this.lblInti.TabIndex = 7;
+            this.lblInti.Text = "Intitulé :";
+            // 
+            // lblCrée
+            // 
+            this.lblCrée.AutoSize = true;
+            this.lblCrée.Location = new System.Drawing.Point(252, 181);
+            this.lblCrée.Name = "lblCrée";
+            this.lblCrée.Size = new System.Drawing.Size(71, 17);
+            this.lblCrée.TabIndex = 6;
+            this.lblCrée.Text = "Créé par :";
+            // 
+            // btnFin
+            // 
+            this.btnFin.Location = new System.Drawing.Point(690, 472);
+            this.btnFin.Name = "btnFin";
+            this.btnFin.Size = new System.Drawing.Size(60, 49);
+            this.btnFin.TabIndex = 5;
+            this.btnFin.Text = ">>";
+            this.btnFin.UseVisualStyleBackColor = true;
+            this.btnFin.Click += new System.EventHandler(this.btnFin_Click);
+            // 
+            // btnSuiv
+            // 
+            this.btnSuiv.Location = new System.Drawing.Point(624, 472);
+            this.btnSuiv.Name = "btnSuiv";
+            this.btnSuiv.Size = new System.Drawing.Size(60, 49);
+            this.btnSuiv.TabIndex = 4;
+            this.btnSuiv.Text = ">";
+            this.btnSuiv.UseVisualStyleBackColor = true;
+            this.btnSuiv.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnPre
+            // 
+            this.btnPre.Location = new System.Drawing.Point(558, 472);
+            this.btnPre.Name = "btnPre";
+            this.btnPre.Size = new System.Drawing.Size(60, 49);
+            this.btnPre.TabIndex = 3;
+            this.btnPre.Text = "<";
+            this.btnPre.UseVisualStyleBackColor = true;
+            this.btnPre.Click += new System.EventHandler(this.btnPre_Click);
+            // 
+            // BtnDebut
+            // 
+            this.BtnDebut.Location = new System.Drawing.Point(492, 472);
+            this.BtnDebut.Name = "BtnDebut";
+            this.BtnDebut.Size = new System.Drawing.Size(60, 49);
+            this.BtnDebut.TabIndex = 2;
+            this.BtnDebut.Text = "<<";
+            this.BtnDebut.UseVisualStyleBackColor = true;
+            this.BtnDebut.Click += new System.EventHandler(this.BtnDebut_Click);
+            // 
+            // pgb_Prog
+            // 
+            this.pgb_Prog.Location = new System.Drawing.Point(558, 552);
+            this.pgb_Prog.Name = "pgb_Prog";
+            this.pgb_Prog.Size = new System.Drawing.Size(126, 23);
+            this.pgb_Prog.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.groupBox1.Controls.Add(this.lblNumChange);
+            this.groupBox1.Controls.Add(this.lblNévé);
+            this.groupBox1.Controls.Add(this.ptb_événement);
+            this.groupBox1.Location = new System.Drawing.Point(-4, -36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1235, 176);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblNumChange
+            // 
+            this.lblNumChange.AutoSize = true;
+            this.lblNumChange.Font = new System.Drawing.Font("Yu Gothic UI Semilight", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumChange.Location = new System.Drawing.Point(554, 88);
+            this.lblNumChange.Name = "lblNumChange";
+            this.lblNumChange.Size = new System.Drawing.Size(38, 54);
+            this.lblNumChange.TabIndex = 2;
+            this.lblNumChange.Text = "1";
+            // 
+            // lblNévé
+            // 
+            this.lblNévé.AutoSize = true;
+            this.lblNévé.Font = new System.Drawing.Font("Yu Gothic UI Semilight", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNévé.Location = new System.Drawing.Point(250, 88);
+            this.lblNévé.Name = "lblNévé";
+            this.lblNévé.Size = new System.Drawing.Size(284, 54);
+            this.lblNévé.TabIndex = 1;
+            this.lblNévé.Text = "Evénement n° :";
+            // 
+            // ptb_événement
+            // 
+            this.ptb_événement.Location = new System.Drawing.Point(170, 77);
+            this.ptb_événement.Name = "ptb_événement";
+            this.ptb_événement.Size = new System.Drawing.Size(74, 67);
+            this.ptb_événement.TabIndex = 0;
+            this.ptb_événement.TabStop = false;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 28);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(1231, 639);
+            this.tabPage4.TabIndex = 1;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(0, 0);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(200, 100);
+            this.tabPage1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(0, 0);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(200, 100);
+            this.tabPage2.TabIndex = 0;
+            // 
+            // lblDeb
+            // 
+            this.lblDeb.AutoSize = true;
+            this.lblDeb.Location = new System.Drawing.Point(292, 355);
+            this.lblDeb.Name = "lblDeb";
+            this.lblDeb.Size = new System.Drawing.Size(56, 17);
+            this.lblDeb.TabIndex = 17;
+            this.lblDeb.Text = "**/**/****";
+            // 
+            // lblFin
+            // 
+            this.lblFin.AutoSize = true;
+            this.lblFin.Location = new System.Drawing.Point(521, 355);
+            this.lblFin.Name = "lblFin";
+            this.lblFin.Size = new System.Drawing.Size(56, 17);
+            this.lblFin.TabIndex = 18;
+            this.lblFin.Text = "**/**/****";
+            // 
+            // événement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1222, 661);
+            this.Controls.Add(tbcEvé);
+            this.Name = "événement";
+            this.Text = "événement";
+            tbcEvé.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_événement)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnFin;
+        private System.Windows.Forms.Button btnSuiv;
+        private System.Windows.Forms.Button btnPre;
+        private System.Windows.Forms.Button BtnDebut;
+        private System.Windows.Forms.ProgressBar pgb_Prog;
+        private System.Windows.Forms.Label lblNumChange;
+        private System.Windows.Forms.Label lblNévé;
+        private System.Windows.Forms.PictureBox ptb_événement;
+        private System.Windows.Forms.Label lblFini;
+        private System.Windows.Forms.Label lblau;
+        private System.Windows.Forms.Label lbldu;
+        private System.Windows.Forms.Label lblDescri;
+        private System.Windows.Forms.Label lblInti;
+        private System.Windows.Forms.Label lblCrée;
+        private System.Windows.Forms.RadioButton rdbRéglé;
+        private System.Windows.Forms.Label LblEveDescri;
+        private System.Windows.Forms.Label LblEveInt;
+        private System.Windows.Forms.Label LblCréateur;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblFin;
+        private System.Windows.Forms.Label lblDeb;
+    }
+}
---
+++ Projet_fin/Projet_fin/événement.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projet_fin
+{
+    public partial class événement : Form
+    {
+        public événement()
+        {
+            InitializeComponent();
+        }
+        int NumReq=1;
+        int NbPage = 5;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (NumReq != NbPage)
+            {
+                NumReq++;
+                lblNumChange.Text = "" + NumReq + " ";
+                pgb_Prog.Value += 25;
+            }
+
+        }
+
+        private void btnPre_Click(object sender, EventArgs e)
+        {
+            if (NumReq != 1 )
+            {
+                NumReq--;
+                lblNumChange.Text = "" + NumReq + " ";
+                pgb_Prog.Value -= 25;
+            }
+        }
+
+        private void BtnDebut_Click(object sender, EventArgs e)
+        {
+            NumReq = 1;
+            lblNumChange.Text = "" + NumReq + " ";
+            pgb_Prog.Value = 0;
+        }
+
+        private void btnFin_Click(object sender, EventArgs e)
+        {
+            NumReq = NbPage;
+            lblNumChange.Text = "" + NumReq + " ";
+            pgb_Prog.Value = 100;
+        }
+
+
+
+    }
+}
