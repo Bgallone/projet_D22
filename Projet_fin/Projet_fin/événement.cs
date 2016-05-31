247de1c5fc0182e95f969a5acef96b90fdf97fcb
Stodin <flopo57230@hotmail.fr>
2016-05-31 17:15:49 +0200

    J'ai presque terminé

--- Projet_fin/Projet_fin/événement.cs
+++ Projet_fin/Projet_fin/événement.cs
@@ -23,26 +23,20 @@ namespace Projet_fin
             InitializeComponent();
         }
         int NumReq=1;
-        int NbPage = 5;
+        int NbPage;
         private void événement_Load(object sender, EventArgs e)
         {
-            string res = "";
             co.ConnectionString = chco;
             co.Open();
             OleDbCommand cmd = new OleDbCommand();
             cmd.Connection = co;
             cmd.CommandType = CommandType.Text;
-            string req = @"SELECT [titreEvent] 
-                            FROM Evenements
-                WHERE codeEvent = "+NumReq+";";
+            string req = @"SELECT count(*)
+                            FROM Evenements;";
             cmd.CommandText = req;
-            res = cmd.ExecuteScalar().ToString();
-            LblEveInt.Text = res;
-            req = @"SELECT [nomPart p]
-                    FROM Participants p
-                    WHERE codeParticipant =(SELECT [codeCreateur]
-                                            FROM Evenements
-                                            WHERE codeEvent = " + NumReq + ");";
+            NbPage = (int)cmd.ExecuteScalar();
+            co.Close();
+            remplir( NumReq);
         
         }
         private void button3_Click(object sender, EventArgs e)
@@ -50,6 +44,7 @@ namespace Projet_fin
             if (NumReq != NbPage)
             {
                 NumReq++;
+                remplir(NumReq);
                 lblNumChange.Text = "" + NumReq + " ";
                 pgb_Prog.Value += 100/(NbPage-1);
             }
@@ -61,6 +56,7 @@ namespace Projet_fin
             if (NumReq != 1 )
             {
                 NumReq--;
+                remplir(NumReq);
                 lblNumChange.Text = "" + NumReq + " ";
                 pgb_Prog.Value -= 100/(NbPage-1);
             }
@@ -68,19 +64,81 @@ namespace Projet_fin
 
         private void BtnDebut_Click(object sender, EventArgs e)
         {
-            NumReq = 1;
-            lblNumChange.Text = "" + NumReq + " ";
-            pgb_Prog.Value = 0;
+            if (NumReq != 1)
+            {
+                NumReq = 1;
+                remplir(NumReq);
+                lblNumChange.Text = "" + NumReq + " ";
+                pgb_Prog.Value = 0;
+            }
         }
 
         private void btnFin_Click(object sender, EventArgs e)
         {
             NumReq = NbPage;
+            remplir(NumReq);
             lblNumChange.Text = "" + NumReq + " ";
             pgb_Prog.Value = 100;
         }
 
+        private void remplir(int Numero_req){
 
+            string res = "";
+            co.ConnectionString = chco;
+            co.Open();
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.Connection = co;
+            cmd.CommandType = CommandType.Text;
+            
+            // met le nom de l'événement
+            string req = @"SELECT [titreEvent] 
+                            FROM Evenements
+                WHERE codeEvent = "+NumReq+";";
+
+            cmd.CommandText = req;
+            res = cmd.ExecuteScalar().ToString();
+            LblEveInt.Text = res;
+
+            // met le nom du créateur 
+            req = @"SELECT [p.nomPart]
+                    FROM Participants p
+                    WHERE p.codeParticipant =(SELECT [codeCreateur]
+                                            FROM Evenements
+                                            WHERE codeEvent = "+NumReq+");";
+            cmd.CommandText = req;
+            res = cmd.ExecuteScalar().ToString();
+            LblCréateur.Text = res;
+            
+            // met la description de l'événement 
+
+            req = @"SELECT [description]
+                    FROM Evenements
+                    WHERE codeEvent = " + NumReq +";";
+            cmd.CommandText = req;
+            res = cmd.ExecuteScalar().ToString();
+            LblEveDescri.Text = res;
+
+            // met la date de debut 
+            req = @"SELECT [dateDebut]
+                    FROM Evenements
+                    WHERE codeEvent = " + NumReq + ";";
+            cmd.CommandText = req;
+            res = cmd.ExecuteScalar().ToString();
+            lblDeb.Text = res;
+           
+            //met la date de fin 
+            req = @"SELECT [dateFin]
+                    FROM Evenements
+                    WHERE codeEvent = " + NumReq + ";";
+            cmd.CommandText = req;
+            res = cmd.ExecuteScalar().ToString();
+            lblFin.Text = res;
+
+            //coche si l'événement est réglée 
+
+            co.Close();
+
+        }
 
 
     }
