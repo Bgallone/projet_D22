06774dded32c54d84b48f59fd699103141de5349
Stodin <flopo57230@hotmail.fr>
2016-05-31 16:32:05 +0200

    Avancer

--- Projet_fin/Projet_fin/événement.cs
+++ Projet_fin/Projet_fin/événement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,12 +13,33 @@ namespace Projet_fin
 {
     public partial class événement : Form
     {
+
+        String chco = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=./Resources/bdEvents.mdb";
+        OleDbConnection co = new OleDbConnection();
+        DataSet ds = new DataSet();
+
         public événement()
         {
             InitializeComponent();
         }
         int NumReq=1;
         int NbPage = 5;
+        private void événement_Load(object sender, EventArgs e)
+        {
+            string res = "";
+            co.ConnectionString = chco;
+            co.Open();
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.Connection = co;
+            cmd.CommandType = CommandType.Text;
+            string req = @"SELECT [titreEvent] 
+                            FROM Evenements
+                WHERE codeEvent = '"+NumReq+"';";
+            cmd.CommandText = req;
+            res = cmd.ExecuteScalar().ToString();
+            LblEveInt.Text = res;
+        
+        }
         private void button3_Click(object sender, EventArgs e)
         {
             if (NumReq != NbPage)
@@ -52,6 +74,7 @@ namespace Projet_fin
             lblNumChange.Text = "" + NumReq + " ";
             pgb_Prog.Value = 100;
         }
+
 
 
 
