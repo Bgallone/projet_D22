96f61d59607a5be452afd9c32ccfc8d332ffbe8f
arthur.klipfel <arthur.klipfel@outlook.fr>
2016-06-01 14:41:39 +0200

    j

--- Projet_fin/Projet_fin/Program.cs
+++ Projet_fin/Projet_fin/Program.cs
@@ -17,6 +17,7 @@ namespace Projet_fin
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmLancement());
+            
         }
     }
 }
