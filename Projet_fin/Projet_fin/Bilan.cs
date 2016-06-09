d41d6b44f9329d45629962cd039f7f257715005c
arthur.klipfel <arthur.klipfel@outlook.fr>
2016-06-09 20:25:07 +0200

    Beau jeu

--- Projet_fin/Projet_fin/Bilan.cs
+++ Projet_fin/Projet_fin/Bilan.cs
@@ -108,23 +108,7 @@ namespace Projet_fin
             co.Open();
             
             DataTable dt = new DataTable();
-            DataColumn dc1 = new DataColumn();
-            dc1.DataType = System.Type.GetType("System.Int32");
-            dc1.ColumnName = "Numéro Depense";
-            dt.Columns.Add(dc1);
-            DataColumn dc2 = new DataColumn();
-            dc2.DataType = Type.GetType("System.String");
-            dc2.ColumnName = "date";
-            dt.Columns.Add(dc2);
-            DataColumn dc3 = new DataColumn();
-            dc3.DataType = Type.GetType("System.String");
-            dc3.ColumnName = "description";
-            dt.Columns.Add(dc3);
-            DataColumn dc4 = new DataColumn();
-            dc4.DataType = Type.GetType("System.Decimal");
-            dc4.ColumnName = "montant";
-            dt.Columns.Add(dc4);
-
+   
             string reqpart = @"SELECT codeParticipant
                               FROM Participants
                               WHERE nomPart = '" + cbxParticipant.Text + "';";
@@ -155,24 +139,9 @@ namespace Projet_fin
             OleDbDataAdapter da = new OleDbDataAdapter(cmd);
             da.Fill(dt);
             dgvDépensé.DataSource = dt;
-          
-
 
             DataTable dt2 = new DataTable();
-            DataColumn c1 = new DataColumn();
-            c1.DataType = System.Type.GetType("System.Int32");
-            c1.ColumnName = "Numéro Depense";
-            dt2.Columns.Add(c1);
-            DataColumn c2 = new DataColumn();
-            c2.DataType = Type.GetType("System.Decimal");
-            c2.ColumnName = "montant";
-            dt2.Columns.Add(c2);
-            DataColumn c3 = new DataColumn();
-            c3.DataType = Type.GetType("System.Int32");
-            c3.ColumnName = "SommeDenbPart";
-            dt2.Columns.Add(c3);
-            
-
+          
             OleDbParameter parampart = new OleDbParameter();
             parampart.ParameterName = "@pPart";
             parampart.OleDbType = OleDbType.Integer;
@@ -194,7 +163,7 @@ namespace Projet_fin
             cmd2.Parameters.Add(paramevent);
             cmd2.Parameters.Add(parampart);
             OleDbDataAdapter da2 = new OleDbDataAdapter(cmd2);
-            da.Fill(dt2);
+            da2.Fill(dt2);
             dgvArembourser.DataSource = dt2;
            
             co.Close();
@@ -205,7 +174,7 @@ namespace Projet_fin
 
         private void btnCloture_Click(object sender, EventArgs e)
         {
-            OleDbCommand cmd = new OleDbCommand();
+           OleDbCommand cmd = new OleDbCommand();
             cmd.Connection = co;
             co.ConnectionString = chco;
             co.Open();
@@ -235,7 +204,7 @@ namespace Projet_fin
 
 
 
-            while (dr.Read())
+           while (dr.Read())
             {
                 int NumPart = dr.GetInt32(0);
                 DepenseCredit(NumEve,NumPart) ;
@@ -243,11 +212,11 @@ namespace Projet_fin
 
 
             co.Close();
-            /*string req = @"UPDATE Evenements
+            string req = @"UPDATE Evenements
                            SET soldeON = True
                            WHERE titreEvent = '" + cbxEvenement.Text + "';";
             OleDbCommand cmd = new OleDbCommand(req, co);
-            MessageBox.Show(cmd.ExecuteNonQuery().ToString());*/
+            MessageBox.Show(cmd.ExecuteNonQuery().ToString());
         }
         private int DepenseCredit(int codeEvt,int numeroParticipant)
         {
